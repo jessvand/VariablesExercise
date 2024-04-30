dbd925af52a129c9ff03dc196fce935cc7a5c4e6
jessvand <jmvandbeek@gmail.com>
2024-04-30 14:35:46 -0400

    Added Puppus Facts

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿//namespace VariablesExercise
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+            string dogName = "Puppus";
+            int dogAge = 12;
+            double avg = 24.00;
+            decimal vetBill = 3000.00m;
+            bool isMyWholeLife = true;
+            char initial = 'P';
+
+            Console.WriteLine($"{dogName}\nAge: {dogAge}\nAverageLifetime:{24}\nCost: {vetBill}\nTheBest: {isMyWholeLife}\nInitial: {initial}");
+
+//        }
+//    }
+//}
